f2602c97b23617e43e0ce639a00e7a984ff9ed53
Arthur A. <arthurakilalves@gmail.com>
2025-09-13 14:17:06 -0300

    feat: criando os models admin

--- api-ecommerce/app/admin.py
+++ api-ecommerce/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Product, Cart, CartItem
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock", "category")
+    search_fields = ("name", "price", "category")
+    list_filter = ("category",)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    search_fields = ("user",)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("cart", "product", "quantity")
+
