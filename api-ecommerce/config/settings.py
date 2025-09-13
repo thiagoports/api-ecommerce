f8c74a84ef25cf5e8649f90c2de3ca3681fb3cbd
Arthur A. <arthurakilalves@gmail.com>
2025-09-13 14:08:07 -0300

    fix: ajustando o env no settings e resolvendo bugs

--- api-ecommerce/config/settings.py
+++ api-ecommerce/config/settings.py
@@ -35,11 +35,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#$3xcss*=-gvhd4g=4-6xddm&u(9nwm&@rr%=fr%sut*y%=7m'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
