178cc478bf4c764bc07716d4776e2d4b1885d10f
José Lucas <jlgf.profissional@gmail.com>
2025-09-17 12:31:58 -0300

    feat: payment models

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .viewsets import CategoryViewSet, ProductViewSet, CartViewSet, CartItemViewSet
+from .viewsets import CategoryViewSet, ProductViewSet, CartViewSet, CartItemViewSet, PaymentViewSet
 
 # Router: Gera as URLs para as viewsets automaticamente.
 
@@ -8,6 +8,7 @@ routerv1 = DefaultRouter()
 routerv1.register(r'categories', CategoryViewSet)
 routerv1.register(r'products', ProductViewSet)
 routerv1.register(r'carts', CartViewSet)
-routerv1.register(r'cart-items',CartItemViewSet)
+routerv1.register(r'cart-items', CartItemViewSet)
+routerv1.register(r'payments', PaymentViewSet)
 
 urlpatterns = routerv1.urls
