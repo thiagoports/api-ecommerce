0ac6092d6d70bb85f494b567fe25b38646700afa
Arthur A. <arthurakilalves@gmail.com>
2025-09-17 12:47:46 -0300

    fix: redirecionamento na url a caminho da rota para api/v1

--- config/router/api_v1.py
+++ config/router/api_v1.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include
 from django.urls import path
 
-from app.api.v1.router import routerv1
-
 # Aqui puxamos as rotas da pasta v1, ou seja, da versão 1 do nosso aplicativo (app), qualquer alteração ou coisa a mais que quisermos colocar ainda na v1 de ROTA podemos colocar aqui que não influencia em outras rotas de outras versões
 
 api_v1_urls = [
-    path("", include((routerv1.urls, "v1"), namespace="v1"))
-    ]
+    # A função include() pode receber o caminho para um módulo de URLs.
+    # O Django irá procurar automaticamente pela variável 'urlpatterns'
+    # dentro de 'app.api.v1.router' e incluir todas as rotas de lá.
+    path("", include("app.api.v1.router")),
+]
